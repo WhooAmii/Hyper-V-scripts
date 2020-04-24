709f8179fa249270611d1baeec645f7f984d2ba3
gerhart01 <25981258+gerhart01@users.noreply.github.com>
2020-04-24 19:15:16 +0300

    Update ParseAfdEndpointListHead.py and ParseAfdTlTransportListHead.py to Python3.

--- ParseAfdEndpointListHead.py
+++ ParseAfdEndpointListHead.py
@@ -1,9 +1,6 @@
 __author__ = "Gerhart"
 __license__ = "GPL3"
-__version__ = "0.0.1"
-
-#based on http://www.codemachine.com/article_findafdendpoints.html
-#list afd endpoints with some additional info. Hyper-V sockets endpoints are included.
+__version__ = "1.2.0"
 
 from pykd import *
 import sys
@@ -11,12 +8,12 @@ import sys
 
 afd = module("afd")
 ListHead = afd.AfdEndpointListHead
-print "afd!AfdEndpointListHead address is ",hex(ListHead)
+print("afd!AfdEndpointListHead address is ", hex(ListHead))
 ptrNext = ptrQWord(ListHead)
-print "----AfdEndpoint",hex(ListHead),hex(ptrWord(ListHead-0x120))
+print("----AfdEndpoint", hex(ListHead), hex(ptrWord(ListHead-0x120)))
 count = 1
-while (ptrNext <> ListHead) & (ptrNext != 0xffffffffffffffffL):
-	print "----AfdEndpoint",hex(ptrNext),hex(ptrWord(ptrNext-0x120)),findSymbol(ptrQWord(ptrNext-0x108)),loadCStr(ptrQWord(ptrNext-0x120+0x28)+0x450),hex(ptrQWord(ptrQWord(ptrNext-0x120+0x28)+0x2e8))
+while (ptrNext != ListHead) & (ptrNext != 0xffffffffffffffff):
+	print("----AfdEndpoint",hex(ptrNext), hex(ptrWord(ptrNext-0x120)), findSymbol(ptrQWord(ptrNext-0x108)), loadCStr(ptrQWord(ptrNext-0x120+0x28)+0x450), hex(ptrQWord(ptrQWord(ptrNext-0x120+0x28)+0x2e8)))
 	ptrNext = ptrQWord(ptrNext)
 	count = count+1
-print "Cycle end. Count", count
+print("Cycle end. Count", count)
--- ParseAfdTlTransportListHead.py
+++ ParseAfdTlTransportListHead.py
@@ -1,11 +1,10 @@
+__author__ = "Gerhart"
+__license__ = "GPL3"
+__version__ = "1.2.0"
+
 from pykd import *
 import sys
 
-__author__ = "Gerhart"
-__license__ = "GPL3"
-__version__ = "0.0.1"
-
-#parsing AfdTlTransportListHead structure
 
 AF_dict = {
  0:"AF_UNSPEC", 
@@ -45,13 +44,23 @@ AF_dict = {
  35:"AF_MAX"
 }
 
-afd = module("afd")
+afd = pykd.module("afd")
 ListHead = afd.AfdTlTransportListHead
+#print "afd!AfdTlTransportListHead address is ",hex(ListHead)
 afd_rbx = ptrQWord(ListHead)
-print "cs:AfdTlTransportListHead address is ",hex(afd_rbx)
+print("cs:AfdTlTransportListHead address is ", hex(afd_rbx))
 
-while (afd_rbx <> ListHead) & (afd_rbx != 0xffffffffffffffffL):
-	print "----Address family",hex(ptrByte(afd_rbx+0x16)),"[",AF_dict[ptrByte(afd_rbx+0x16)],"]"
-	print "--Dispatch function",findSymbol(ptrQWord(afd_rbx+0x28))
-	afd_rbx = ptrQWord(afd_rbx)
-print "Cycle end. Afd_rbx",hex(afd_rbx)
+while (afd_rbx != ListHead) & (afd_rbx != 0xffffffffffffffff):
+ #print "afd_rbx in cycle",hex(afd_rbx)
+ #print "AfdTlHead+0x16",hex(ptrQWord(afd_rbx+0x16))
+ #print "AfdTlHead+0x1c",hex(ptrQWord(afd_rbx+0x1c))
+ #print "----Address family",hex(ptrByte(afd_rbx+0x16))
+    print("----Address family", hex(ptrByte(afd_rbx + 0x16)), "[", AF_dict[ptrByte(afd_rbx + 0x16)], "]")
+    print("--Dispatch function", findSymbol(ptrQWord(afd_rbx + 0x28)))
+    afd_rbx = ptrQWord(afd_rbx)
+print("Cycle end. Afd_rbx", hex(afd_rbx))
+
+#afd = dbgCommand("? afd!AfdTlTransportListHead")
+#afd = typedVar("afd!AfdTlTransportListHead")
+#afd = expr("@rax+10")
+#afd = exprmodule("afd")
